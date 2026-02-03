c34af05bbe56dc22fcc4182a5efd08ccbc7e0d04
Deep Saha <dip23447@gmail.com>
2026-02-03 16:17:03 +0530

    Modified code

--- proxy.js
+++ proxy.js
@@ -1,33 +1,68 @@
 const http = require('http');
+const net = require('net');
 const url = require('url');
 
 const PORT = 8080;
 
+/**
+ * HTTP PROXY
+ */
 const server = http.createServer((req, res) => {
-    const parseUrl = url.parse(req.url);
+  try {
+    const parsedUrl = url.parse(req.url);
 
     const options = {
-        hostname: parseUrl.hostname,
-        port: parseUrl.port || 80,
-        path: parseUrl.path,
-        method: req.method,
-        headers: req.headers
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port || 80,
+      path: parsedUrl.path,
+      method: req.method,
+      headers: req.headers
     };
 
+    console.log(`[HTTP] ${req.method} ${req.url}`);
+
     const proxyReq = http.request(options, (proxyRes) => {
-        res.writeHead(proxyRes.statusCode, proxyRes.headers);
-        proxyRes.pipe(res);
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      proxyRes.pipe(res);
     });
 
     req.pipe(proxyReq);
 
     proxyReq.on('error', (err) => {
-        res.writeHead(500);
-        res.end('Proxy Error');
-        console.log(err);
+      console.error('HTTP Proxy Error:', err.message);
+      res.writeHead(500);
+      res.end('Proxy Error');
     });
+
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500);
+    res.end('Internal Error');
+  }
+});
+
+/**
+ * HTTPS (CONNECT) PROXY
+ */
+server.on('connect', (req, clientSocket, head) => {
+  const [host, port] = req.url.split(':');
+
+  console.log(`[HTTPS] CONNECT ${host}:${port}`);
+
+  const serverSocket = net.connect(port || 443, host, () => {
+    clientSocket.write(
+      'HTTP/1.1 200 Connection Established\r\n\r\n'
+    );
+
+    serverSocket.write(head);
+    serverSocket.pipe(clientSocket);
+    clientSocket.pipe(serverSocket);
+  });
+
+  serverSocket.on('error', () => clientSocket.end());
+  clientSocket.on('error', () => serverSocket.end());
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Proxy server running on port ${PORT}`);
 });
